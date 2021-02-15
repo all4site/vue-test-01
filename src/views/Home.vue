a7f7a52f2cd67b84a853c131437799851cc4e928
All4site <35457395+all4site@users.noreply.github.com>
2021-02-15 20:47:29 +0200

    Vue test 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,102 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div v-for="post in posts" :key="post.id" class="post">
+      <h2>{{ post.title | cutTitle | firstToUpperCase }}</h2>
+      <p>{{ post.body }}</p>
+      <div class="post-data">
+        <span>Post id - {{ post.id }}</span>
+        <span>User is - {{ post.userId }}</span>
+      </div>
+      <div class="post-link-wrapper">
+        <router-link :to="{path: `/post/${post.id}`}" class="post-link">PostMore</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data()
+  {
+    return {
+      posts: {}
+    }
+  },
+  mounted()
+  {
+    this.uploadPosts()
+  },
+  methods: {
+    uploadPosts()
+    {
+      axios.get('https://jsonplaceholder.typicode.com/posts').then(response => this.posts = response.data)
+    }
+  },
+  filters: {
+    cutTitle(value)
+    {
+      let title = value.slice(0, 10) + '...'
+      return title
+    },
+    firstToUpperCase(value){
+      if (!value) return value;
+
+      return value[0].toUpperCase() + value.slice(1);
+    }
   }
 }
 </script>
+
+<style scoped>
+h2 {
+  text-align: center;
+  margin-bottom: 2rem;
+}
+
+.wrapper {
+  max-width: 1140px;
+  margin: 3rem auto;
+}
+
+.post {
+  box-shadow: 1px 0 20px 0 #00000030;
+  padding: 2rem;
+  margin-bottom: 2rem;
+}
+
+.post-data {
+  display: flex;
+  flex-direction: row;
+  text-transform: lowercase;
+  color: #37909f;
+  margin-bottom: 2rem;
+}
+
+.post-data span:first-of-type {
+  padding-right: .5rem;
+
+}
+
+.post-link-wrapper {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.post-link {
+  border: 1px solid cornflowerblue;
+  padding: .3rem .8rem;
+  border-radius: 3px;
+  text-decoration: none;
+  color: cornflowerblue;
+  transition: all .5s ease;
+}
+
+.post-link:hover {
+  border-color: #0c45a0;
+  color: #0c45a0;
+}
+
+</style>
---
+++ src/views/Post.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="wrapper">
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.body }}</p>
+    <div class="post-data">
+      <span>Post id - {{ post.id }}</span>
+      <span>User is - {{ post.userId }}</span>
+    </div>
+
+    <div class="post-link-wrapper">
+      <router-link :to="{name: 'Home'}" class="post-link">Home</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Post",
+  data()
+  {
+    return {
+      post: {}
+    }
+  },
+  mounted()
+  {
+    this.postData()
+  },
+  methods: {
+    postData()
+    {
+      axios.get(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`).then(r => this.post = r.data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  max-width: 1140px;
+  margin: 3rem auto;
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 3rem;
+}
+
+.post-link-wrapper {
+  margin-top: 2rem;
+  justify-content: center;
+  display: flex;
+}
+
+.post-link {
+  border: 1px solid cornflowerblue;
+  padding: .3rem .8rem;
+  border-radius: 3px;
+  text-decoration: none;
+  color: cornflowerblue;
+  transition: all .5s ease;
+}
+
+.post-link:hover {
+  border-color: #0c45a0;
+  color: #0c45a0;
+}
+
+.post-data {
+  margin-top: 2rem;
+  text-transform: lowercase;
+}
+
+span {
+  color: #42b983;
+}
+
+.post-data span:first-of-type {
+  margin-right: 1rem;
+  color: red;
+}
+</style>
